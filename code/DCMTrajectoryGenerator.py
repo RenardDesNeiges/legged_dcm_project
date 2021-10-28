a32f4970e91d5f226d4614c1bb8ee5c78d0f0d87
Titouan Renard <titouan.renard@gmail.com>
2021-10-28 16:35:17 +0200

    eq 7 line

--- code/DCMTrajectoryGenerator.py
+++ code/DCMTrajectoryGenerator.py
@@ -33,7 +33,7 @@ class DCMTrajectoryGenerator:
         self.CoM[0] = com_ini
         self.CoMDot[0] = 0
         for kk in range(0,self.CoM.shape[0]-1):
-            self.CoMDot[kk+1]= self.omega*(self.DCM -self.CoM[kk]) #Equation (3)
+            self.CoMDot[kk+1]= self.omega*(self.DCM[kk] -self.CoM[kk]) #Equation (3)
             self.CoM[kk+1]= self.CoM[kk]+self.timeStep*self.CoMDot[kk] #Simple euler numerical integration
             self.CoM[kk+1][2]=self.CoMHeight
         return self.CoM
@@ -52,7 +52,7 @@ class DCMTrajectoryGenerator:
         self.DCMForEndOfStep[-1] = # capturability constraint(3rd item of jupyter notebook steps for DCM motion planning section)
 
         for index in range(np.size(self.CoP,0)-2,-1,-1):
-            self.DCMForEndOfStep[index] = #equation 7 of the jupyter notebook
+            self.DCMForEndOfStep[index] = self.COP + (self.DCMForEndOfStep[index-1] - self.COP) * np.exp(self.omega * self.stepDuration) #equation 7 of the jupyter notebook
         pass
 
     def calculateCoPTrajectory(self):
